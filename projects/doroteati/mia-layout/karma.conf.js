3fb1f429565fee02943939b77c3cd65b8573760e
HeiderHDev <heiderhrnndz@gmail.com>
2024-10-09 15:38:08 -0500

    refactor: 💡 rename to doroteati

--- projects/doroteati/mia-layout/karma.conf.js
+++ projects/doroteati/mia-layout/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../../coverage/agencycoda/mia-layout'),
+      dir: require('path').join(__dirname, '../../../coverage/doroteati/mia-layout'),
       subdir: '.',
       reporters: [
         { type: 'html' },
